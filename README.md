7961dd95e02a544be287008d9b731f87171cf31d
Ad5001 <mail@ad5001.eu>
2017-03-17 20:39:33 +0100

    Uploading finished plugin !

---
+++ README.md
@@ -0,0 +1,19 @@
+# FloatingTexts
+Create floating texts for your PocketMine server !
+
+Welcome your players with a nice, class, and clean message, display infos on large walls easily, and much more in a few steps using this plugin!     
+
+## Downloading the plugin.
+1. Go to https://download.ad5001.eu/en/view.php?src=github&name=FloatingTexts and click "Download".    
+2. After the .phar has been downloaded, put it into your "plugins" folder.
+3. Restart your server.
+
+## Create a floating text.
+1. Spawn any sort of mob (using a spawn egg or /summon)
+2. Then enter command "/createfloat <text to display>". Use \n to go to next line.
+3. Hit the mob you want to transform into a floating text.
+    
+And voilà ! You have created a floating text !  
+
+## License
+For those who didn't know, I always use the [BoxOfDevs Public General License](https://raw.githubusercontent.com/BoxOfDevs/Functions/master/LICENSE) v1.1.3 due to some old problems.
---
+++ src/Ad5001/FloatingTexts/Main.php
@@ -0,0 +1,94 @@
+<?php
+# FloatingTexts
+# A new production from Ad5001 generated using ImagicalPlugCreator by Ad5001 (C) 2017
+
+namespace Ad5001\FloatingTexts;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\entity\Entity;
+
+
+
+class Main extends PluginBase implements \pocketmine\event\Listener {
+
+		/*
+		Called when the plugin enables
+		*/
+		public function onEnable() {
+			$this->getServer()->getPluginManager()->registerEvents($this, $this);
+			$this->sessions = [];
+		}
+
+		/*
+		Called when the plugin disables
+		*/
+		public function onDisable() {
+		}
+
+		/*
+		Called when one of the defined commands of the plugin has been called
+		@param     $sender        \pocketmine\command\CommandSender
+		@param     $cmd           \pocketmine\command\Command
+		@param     $label         mixed
+		@param     $args          array
+		return bool
+		*/
+		public function onCommand(\pocketmine\command\CommandSender $sender, \pocketmine\command\Command $cmd, $label, array $args): bool {
+			switch($cmd->getName()) {
+				case "createfloat":
+				if($sender instanceof Player) {
+					if(isset($args[0])) {
+						$text = implode(" ", $args);
+						$text = str_ireplace("\\n", "\n", $text);
+						$this->sessions[$sender->getName()] = $text;
+						$sender->sendMessage("Tap an entity !");
+					}
+				}
+				return true;
+				break;
+			}
+		}
+
+		/*
+		WHen a player hits an entity with a session, set his nametag
+		@param     $event    \pocketmine\event\entity\EntityDamageEvent
+		*/
+		public function onEntityDamage(\pocketmine\event\entity\EntityDamageEvent $event) {
+			if($event instanceof \pocketmine\event\entity\EntityDamageByEntityEvent) {
+				if($event->getDamager() instanceof Player &&
+				isset($this->sessions[$event->getDamager()->getName()])) {
+					$event->getEntity()->addEffect(\pocketmine\entity\Effect::getEffectByName("invisibility")->setDuration(99999)->setVisible(false));
+					$event->getEntity()->setNameTag($this->sessions[$event->getDamager()->getName()]);
+                	$event->getEntity()->setNameTagAlwaysVisible(true);
+            		$event->getEntity()->setNameTagVisible(true);
+            		$event->getEntity()->setImmobile(true);
+					$event->getEntity()->namedtag->isUsedToFloat = new \pocketmine\nbt\tag\StringTag("isUsedToFloat", "true");
+					$event->getEntity()->setNameTag($this->sessions[$event->getDamager()->getName()]);
+					$event->setCancelled();
+					unset($this->sessions[$event->getDamager()->getName()]);
+				} elseif(isset($event->getEntity()->namedtag->isUsedToFloat)) {
+					if(!($event->getDamager() instanceof Player && $event->getDamager()->isOp())) $event->setCancelled();
+				}
+			} elseif(isset($event->getEntity()->namedtag->isUsedToFloat)) {
+				$event->setCancelled();
+			}
+		}
+
+
+		/*
+		Checks when a level loads with floats to regive them the flags and effects.
+		@param     $event    \pocketmine\event\level\LevelLoadEvent
+		*/
+		public function onLevelLoad(\pocketmine\event\level\LevelLoadEvent $event) {
+			foreach ($event->getLevel()->getEntities() as $et) {
+				if(isset($event->getEntity()->namedtag->isUsedToFloat)) {
+					$event->getEntity()->addEffect(\pocketmine\entity\Effect::getEffectByName("invisibility")->setDuration(99999)->setVisible(false));
+                	$event->getEntity()->setNameTagAlwaysVisible(true);
+            		$event->getEntity()->setNameTagVisible(true);
+            		$event->getEntity()->setImmobile(true);
+				}
+			}
+		}
+}
